e2bd0810233cbd5ff92e272dc719334a6aad2180
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-05-25 02:52:03 +0300

    add imports in default module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DefaultComponent } from './layouts/default/default.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefaultComponent
+
   ],
   imports: [
     BrowserModule,
