43c826c56adcc19ec582c7ed5b2bf98fd15a9079
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-05-25 02:47:24 +0300

    add default layout component and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { DefaultComponent } from './layouts/default/default.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
