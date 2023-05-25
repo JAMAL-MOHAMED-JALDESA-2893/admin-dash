906584f82b9148986dbb85c0093b2c69ae9873c0
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-05-25 07:15:14 +0300

    add update

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,7 +4,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MatDividerModule } from '@angular/material/divider';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     CommonModule,
     MatDividerModule,
     MatToolbarModule,
+    MatIconModule,
   ],
   exports: [
     HeaderComponent,
