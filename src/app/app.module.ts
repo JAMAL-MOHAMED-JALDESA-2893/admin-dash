17eaf7c7dee0d8a6abb747620449c43f8cf5117b
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-05-25 03:09:09 +0300

    add update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultModule } from './layouts/default/default.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-
+    DefaultModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
