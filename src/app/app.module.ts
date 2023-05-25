482229e892a8185a534dce0c25431f30c7fec580
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-05-25 03:24:03 +0300

    add posts component to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { PostsComponent } from './modules/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent,
+    PostsComponent,
 
   ],
   imports: [
